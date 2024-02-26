6f1742ca899b5b55ca08a84784e1713f57b0963b
PriyankaNigade18 <Piyu1818@gmail.com>
2024-02-26 19:15:07 +0530

    26-02-2024 git branches

---
+++ src/test/java/gitdemo2/GitTestWithBranch.java
@@ -0,0 +1,12 @@
+package gitdemo2;
+
+import org.testng.annotations.Test;
+
+public class GitTestWithBranch {
+  @Test
+  public void f() 
+  {
+	  System.out.println("This is for git branch testing");
+	  
+  }
+}
